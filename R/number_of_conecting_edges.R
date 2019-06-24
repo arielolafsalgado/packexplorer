1c0e60a2b90ecb3e9a7a06a57d0810c27879815b
arielolafsalgado <42683165+arielolafsalgado@users.noreply.github.com>
2019-06-24 16:39:55 -0300

    Update number_of_conecting_edges.R

--- R/number_of_conecting_edges.R
+++ R/number_of_conecting_edges.R
@@ -1,10 +1,10 @@
-#' Gets the number of conecting edges between two groups
+#' Get the number of conecting edges between two groups
 #'
 #' It calculates the number of edges from group1 to group2
 #' @param g The graph object
 #' @param group1 The departure set of nodes
 #' @param group2 The arrival set of nodes
-#' @param mode The direction of the edges to be considered. If 'in', the edges considered will be incident over group1, if 'out', will be incident over group2, if 'all', all edges are considered.
+#' @param mode The direction of the edges to be considered. If 'in', the edges considered will be incidents over group1, if 'out', will be incident over group2, if 'all', all edges are considered.
 #' @export
 
 number_of_conecting_edges <- function(g,group1,group2,mode='all'){
