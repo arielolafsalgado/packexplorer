d28468511e7e964db79aac5ef5b67015d238738e
arielolafsalgado <arielolafsalgado@gmail.com>
2019-10-03 12:44:29 -0300

    cut_string: texto->text, importsFrom stringr

--- R/cut_string.R
+++ R/cut_string.R
@@ -3,25 +3,25 @@
 #' These functions help to manage the text in the leaflet plotting
 #' @param graph The graph to be plotted.
 #' @param layout The layout of the graph 
-#' @param texto The text to be parsed.
+#' @param text The text to be parsed.
 #' @param nwords The number of words per line.
 #' @param pack The package of interest
 #' @return Multiple values depending on the function
 #' 
 #' @export
 #' @examples
-
-cut_string <- function(texto,nwords=15){
+#' @importFrom stringr str_split
+cut_string <- function(text,nwords=15){
 	data('des')
 	data('cats')
 	data('downloads')
-	if(is.null(texto)) texto = ''
-	if(length(texto)==0) texto = ''
-	sst <- stringr::str_split(texto, " ")[[1]]
+	if(is.null(text)) text = ''
+	if(length(text)==0) text = ''
+	sst <- str_split(text, " ")[[1]]
 	sst = gsub(pattern='<p>|</p>',replacement = '', x= sst)
 	n = length(sst)
 	if(n<nwords){
-		return(texto)
+		return(text)
 	}
 	q = as.integer(n/nwords) + 1	
 	begin= 1
